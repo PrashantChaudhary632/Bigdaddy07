5650ac1220965f9826d87b52e61c3a84ced26697
Bigdaddy07 <94627893+Bigdaddy07@users.noreply.github.com>
2022-05-22 08:27:37 +0545

    Add files via upload

---
+++ inputfromuser.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class inputfromuser{
+	public static void main(String[] args){
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter First Number");
+			int firstnum=scn.nextInt();
+		System.out.println("Enter Second Number");
+			int secondnum=scn.nextInt();
+			int sum=firstnum+secondnum;
+		System.out.println("Sum="+sum);
+}
+}
+		
